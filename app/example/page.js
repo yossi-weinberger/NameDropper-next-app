711f2c73a28a2442babd5f4d711f183df413a7a2
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-04-03 11:48:14 +0300

    fix

--- app/example/page.js
+++ app/example/page.js
@@ -1,16 +1,16 @@
-import "./page.css";
+// import "./page.css";
 
-export default function Home() {
-  return (
-    <div className="main">
-      <p className="title">
-        Iconic plumber, mushroom kingdom, Bowser's nemesis, hero.
-      </p>
-      {/* <img className="pic" src="@/public/next.svg"></img> */}
-      <img
-        className="mario"
-        src="https://interactivechaos.com/sites/default/files/2023-02/super_mario.png"
-      ></img>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className="main">
+//       <p className="title">
+//         Iconic plumber, mushroom kingdom, Bowsers nemesis, hero.
+//       </p>
+//       {/* <img className="pic" src="@/public/next.svg"></img> */}
+//       <img
+//         className="mario"
+//         src="https://interactivechaos.com/sites/default/files/2023-02/super_mario.png"
+//       ></img>
+//     </div>
+//   );
+// }
