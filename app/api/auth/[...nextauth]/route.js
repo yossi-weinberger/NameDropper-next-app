8a5d116ec90acb044585b0e6eaaeffcf29829e12
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-06-02 21:05:04 +0300

    google login added

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import allowedUsers from "./allowedUsers.json";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async authorize(credentials, req) {
+      const { email } = credentials;
+      console.log("User Email:", email);
+      console.log("Allowed Users:", allowedUsers);
+
+      if (allowedUsers.includes(email)) {
+        console.log("Access granted for", email);
+        return true;
+      } else {
+        console.log("Access denied for", email);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
+
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
+
+// export { handler as GET, handler as POST };
